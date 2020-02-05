ce0f2ef600637339251ae48b6da4fede4ed32d0b
Shahzeb <shahzebsheikh54@gmail.com>
2020-02-05 09:35:03 -0600

    First commit, did you try switching it to wumbo

---
+++ height_to_centimeters.js
@@ -0,0 +1,94 @@
+// var num1 = prompt("pick a number between 1 & 3");
+
+
+
+// function myFirstFunction(){
+// 	var cat = "lucy";
+// 	for(var i = 0; i < 61; i++){
+// 		console.log("You have drank " + i + "ammount of cold pressed juice!");
+// 	}
+// };
+
+// function larry_function(){
+// 	for(var i = 0; i < 3; i++){
+// 		console.log("Hey you chose Larry's function");
+// 	}
+// }
+
+// if( num1 === "1"){
+// 	myFirstFunction();
+// } else if (num1 === "2"){
+// 	larry_function();
+// } else {
+// 	console.log("nice try buddy, try again...");
+// }
+
+// var x = ["Temor", "Shaz", "wumbo", "Clint"];
+
+// for(var i = 0; i < 4; i++){
+// 	console.log(x[i]);
+// }
+
+function rock_paper_scissors(){
+	var clint = prompt("clint, rock, paper, scissors");
+	var temor = prompt("temor, rock, paper, scissors");
+
+	if (clint === temor) {
+		console.log("it's a tie! Break it up you two!");
+	} else if (clint === "rock") {
+		if(temor === "scissors"){
+			console.log("clint wins");
+		} else if (temor === "paper") {
+			console.log("temor wins");
+		}	
+	} else if (clint === "paper") {
+		if(temor === "rock"){
+			console.log("Clint wins!");
+		} else if (temor === "scissors") {
+			console.log("Temor wins!");
+		}
+	} else if (clint === "scissors") {
+		if(temor === "paper"){
+			console.log("Clint wins");
+		} else if (temor === "rock") {
+			console.log("Temor wins");
+		}
+	} 
+	else {
+		console.log("something broke");
+	}
+}
+
+rock_paper_scissors();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ imperial_to_metric_height.js
@@ -0,0 +1,8 @@
+var a = prompt("what is your name?")
+var x = prompt("what is your favorite color?")
+var y = prompt("what is your favorite food?")
+var z = prompt("what is your favorite desert?")
+
+
+
+console.log(a + "went to a party where there was " y + ", and at the end they ate " + x + " " + z)
